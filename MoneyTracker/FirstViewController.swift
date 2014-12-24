55c991922292683de442561446c602185f228fa7
Vadim Drobinin <vadim@drobinin.com>
2014-12-24 23:12:48 +0300

    Initial commit from local repo

---
+++ MoneyTracker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MoneyTracker
+//
+//  Created by Vadim Drobinin on 24/12/14.
+//  Copyright (c) 2014 Vadim Drobinin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MoneyTracker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MoneyTracker
+//
+//  Created by Vadim Drobinin on 24/12/14.
+//  Copyright (c) 2014 Vadim Drobinin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
